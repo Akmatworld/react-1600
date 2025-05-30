3c9e339df1262c3ac10bdf645bdea18c86628eb3
Akmat <sirazhidinovakmatali@gmail.com>
2025-05-30 18:10:02 +0600

    scss was added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,18 @@ function App() {
 
   return (
     <>
-      <h1>Hello, world</h1>
+    <div className="container">
+      <header className='header'>
+        <nav className='header__nav'>
+          <ul className='header__ul'>
+            <li className='header__li'>Item 1</li>
+            <li className='header__li'>Item <span className='header__span-red'>2</span></li>
+            <li className='header__li'>Item 3</li>
+            <li className='header__li'>Item 4</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
     </>
   )
 }
